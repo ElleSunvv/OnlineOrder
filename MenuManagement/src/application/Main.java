6d4e1a1d043b598f5ad29b91e7a42627473843d3
ElleSunvv <ellesun36@gmail.com>
2023-12-01 12:17:40 -0500

    adds MenuManagement

--- MenuManagement/src/application/Main.java
+++ MenuManagement/src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -19,55 +20,72 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		Button menuListForAdmin = new Button("Menu List");
 		Button categoryList = new Button("Category List");
+		Button userList = new Button("User List");
+		Button workStation = new Button("Work Station");
+		
 		Button menuListForCustomer = new Button("Order Menu");
 		Button orderList = new Button("Order List");
 		
-		VBox navMenu = new VBox();
+		HBox adminMenu = new HBox();
+		HBox customerMenu = new HBox();
 		
-		navMenu.setSpacing(10);
-		navMenu.getChildren().addAll(
-				menuListForAdmin, categoryList, menuListForCustomer, orderList
+		boolean isCustomer = true;
+		boolean isAdmin = true;
+		
+		menuListForAdmin.setVisible(isAdmin);
+		categoryList.setVisible(isAdmin);
+		userList.setVisible(isAdmin);
+		workStation.setVisible(isAdmin);
+		
+		menuListForCustomer.setVisible(isCustomer);
+		orderList.setVisible(isCustomer);
+		
+		adminMenu.setSpacing(10);
+		adminMenu.getChildren().addAll(
+				workStation, menuListForAdmin, categoryList, userList
 				);
 		
-		showAdminScene(primaryStage, setMainPane(navMenu, selectedPane), title);
+		customerMenu.setSpacing(10);
+		customerMenu.getChildren().addAll(menuListForCustomer, orderList);
+		
+
+		showAdminScene(primaryStage, setMainPane(adminMenu, selectedPane, customerMenu), title);
 		
 		menuListForAdmin.setOnAction(e -> {
 			title = "Menu List";
 			selectedPane = new MenuList();
-			showAdminScene(primaryStage, setMainPane(navMenu, selectedPane), title);
+			showAdminScene(primaryStage, setMainPane(adminMenu, selectedPane, customerMenu), title);
 		});
 		
 		categoryList.setOnAction(e -> {
 			title = "Category List";
 			selectedPane = new CategoryList();
-			showAdminScene(primaryStage, setMainPane(navMenu, selectedPane), title);
+			showAdminScene(primaryStage, setMainPane(adminMenu, selectedPane, customerMenu), title);
 		});
 		
 		menuListForCustomer.setOnAction(e -> {
 			title = "Order Menu";
 			selectedPane = new OrderMenu();
-			showCustomerScene(primaryStage, setMainPane(navMenu, selectedPane), title);
+			showCustomerScene(primaryStage, setMainPane(adminMenu, selectedPane, customerMenu), title);
 		});
 		
-		orderList.setOnAction(e -> {
-			try {
-				Parent root = 
-						FXMLLoader.load(getClass().getResource("Main.fxml"));
-				Scene scene = new Scene(root, 400, 400);
-				primaryStage.setTitle("Order List");
-				primaryStage.setScene(scene);
-				primaryStage.show();
-			} catch(Exception ex) {
-				ex.printStackTrace();
-			}
-			
+		workStation.setOnAction(e -> {
+			showSceneBuilder(primaryStage, "Main.fxml", "Work Station");
 		});
+		
+		userList.setOnAction(e -> {
+			showSceneBuilder(primaryStage, "Main.fxml", "User List");
+		});
+		
 	}
 	
-	public Pane setMainPane(Pane navMenu, Pane selectedPane) {
-		HBox mainPane = new HBox();
-		mainPane.getChildren().addAll(navMenu, selectedPane);
-		HBox.setMargin(navMenu, new Insets(10));
+	public Pane setMainPane(Pane adminMenu, Pane selectedPane, Pane customerMenu ) {
+		BorderPane mainPane = new BorderPane();
+		mainPane.setTop(adminMenu);
+		mainPane.setCenter(selectedPane);
+		mainPane.setBottom(customerMenu);
+		BorderPane.setMargin(adminMenu, new Insets(10));
+		BorderPane.setMargin(customerMenu, new Insets(10));
 		return mainPane;
 	}
 	
@@ -79,17 +97,23 @@ public class Main extends Application {
 	}
 	
 	public void showCustomerScene(Stage stage, Pane pane, String title) {
-		Scene scene = new Scene(pane, 400, 500);
+		Scene scene = new Scene(pane, 300, 500);
 		stage.setTitle(title);
 		stage.setScene(scene);
 		stage.show();
 	}
 	
-	public void showSceneBuilder(Stage stage, Parent root, String title) {
-		Scene scene = new Scene(root, 400, 500);
-		stage.setTitle(title);
-		stage.setScene(scene);
-		stage.show();
+	public void showSceneBuilder(Stage stage, String fxmlLink, String title) {
+		try {
+			Parent root = 
+					FXMLLoader.load(getClass().getResource(fxmlLink));
+			Scene scene = new Scene(root, 400, 400);
+			stage.setTitle(title);
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
